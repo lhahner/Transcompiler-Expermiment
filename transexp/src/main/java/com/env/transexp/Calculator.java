e93267d0e021467f5e00da411262b8a9445706a1
lhahner <lennart.hahner@gmail.com>
2024-04-27 14:49:11 +0200

    correction

--- transexp/src/main/java/com/env/transexp/Calculator.java
+++ transexp/src/main/java/com/env/transexp/Calculator.java
@@ -6,19 +6,22 @@ public class Calculator {
 	public int multiply(int x, int y){ 
 		return x*y;
 	}
+	public int sum(int x, int y) {
+		return x+y;
+	}
+	public int divide(int x, int y) {
+		return x/y;
+	}
+	public int subtract(int x, int y) {
+		return x-y;
+	}
 	public boolean OR(byte x, byte y) {
 		return x == 1 || y == 1;
 	}
 	public boolean AND(byte x, byte y) {
 		return (x == 1 && y == 1) || (x == 0 && y == 0);
 	}
-	public String getByteAdress(byte b) {
-		return Integer.toString(Byte.hashCode(b));
-	}
-	public String getPID() {
-		return this.PID;
-	}
-	public String getPROCESS() {
-		return this.PROCESS;
-	}
+	public boolean isOdd(int number) {
+        return number % 2 != 0;
+    }
 }
